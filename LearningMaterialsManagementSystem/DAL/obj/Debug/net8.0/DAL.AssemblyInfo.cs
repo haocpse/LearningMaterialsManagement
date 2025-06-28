3dc21a16bc733d5f207e438661f1e97e0b1aa364
haocp <haocpse184107@fpt.edu.vn>
2025-06-28 21:42:55 +0700

    add dependencies

--- LearningMaterialsManagementSystem/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ LearningMaterialsManagementSystem/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab2c2d68ac7c237afa6400b85e68ebab2364804")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
